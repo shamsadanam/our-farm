e67f1d7567f05f70e6b451fc40cdc9e3bd35ad17
Shamsad Anam <shamsad.anam@gmail.com>
2022-12-10 01:46:50 +0600

    added hero and cat section

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,40 @@
-// let x = 10;
-// console.log(x);
+//catagory slider settings
+$(".cat-slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  touchThreshold: 10,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+      },
+    },
+    {
+      breakpoint: 568,
+      settings: {
+        slidesToShow: 4.5,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 421,
+      settings: {
+        slidesToShow: 3.5,
+        slidesToScroll: 2,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
 
-// let y = 99;
-// console.log(y); 
-
-// let str = "hellos";
+//featured slider settings
